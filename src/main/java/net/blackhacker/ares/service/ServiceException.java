801223e5223975e9eb611468f08fe9503a01c213
BeeHache <bh@blackhacker.net>
2025-12-30 00:46:17 -0500

    Adds ServiceException

---
+++ src/main/java/net/blackhacker/ares/service/ServiceException.java
@@ -0,0 +1,12 @@
+package net.blackhacker.ares.service;
+
+public class ServiceException extends RuntimeException {
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
