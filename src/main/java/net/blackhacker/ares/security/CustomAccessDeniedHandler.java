7389dfac87063cbc6816f661c539298f1645eda3
BeeHache <bh@blackhacker.net>
2026-01-05 20:25:58 -0500

    Clear warnings

--- src/main/java/net/blackhacker/ares/security/CustomAccessDeniedHandler.java
+++ src/main/java/net/blackhacker/ares/security/CustomAccessDeniedHandler.java
@@ -2,6 +2,7 @@ package net.blackhacker.ares.security;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -11,8 +12,8 @@ import java.io.IOException;
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response,
-                       AccessDeniedException accessDeniedException) throws IOException {
+    public void handle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+                       @NonNull AccessDeniedException accessDeniedException) throws IOException {
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.getWriter().write("{\"error\": \"Forbidden\", \"message\": \"You do not have permission to perform this action.\"}");
