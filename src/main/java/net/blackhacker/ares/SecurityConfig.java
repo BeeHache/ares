4820238985aa5627b85ce8f8b365da650c4eb9cd
BeeHache <bh@blackhacker.net>
2026-01-10 21:08:17 -0500

    12 app runs in a container now!

--- src/main/java/net/blackhacker/ares/SecurityConfig.java
+++ src/main/java/net/blackhacker/ares/SecurityConfig.java
@@ -29,7 +29,7 @@ import java.util.Arrays;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    @Value("${spring.profiles.active:dev}")
+    @Value("${spring.profiles.active}")
     private String activeProfile;
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
